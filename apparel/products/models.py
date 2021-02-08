5ecfedd322bcce860671fa472b1839085f5a7f38
RonakNandanwar26 <ronaknandanwar10@gmail.com>
2021-02-08 12:52:01 +0530

    you can add video in products

--- apparel/products/models.py
+++ apparel/products/models.py
@@ -39,6 +39,12 @@ def upload_product_image_path(instance, filename):
     return "Products/{final_filename}".format(final_filename=final_filename)
 
 
+def upload_product_video_path(instance, filename):
+    new_filename = instance.name
+    ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "Products/Video/{final_filename}".format(final_filename=final_filename)
+
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -62,13 +68,14 @@ class Product(models.Model):
     smart2 = ImageSpecField(source='image2', processors=[ResizeToFill(250, 350)], format='JPEG', options={'quality': 99})
     smart3 = ImageSpecField(source='image3', processors=[ResizeToFill(250, 350)], format='JPEG', options={'quality': 99})
     smart4 = ImageSpecField(source='image4', processors=[ResizeToFill(250, 350)], format='JPEG', options={'quality': 99})
+    video1 = models.FileField(default='default.mp4', upload_to=upload_product_video_path, null=True, blank=True)
+    video2 = models.FileField(default='default.mp4', upload_to=upload_product_video_path, null=True, blank=True)
+    video3 = models.FileField(default='default.mp4', upload_to=upload_product_video_path, null=True, blank=True)
+    video4 = models.FileField(default='default.mp4', upload_to=upload_product_video_path, null=True, blank=True)
 
 
     def __str__(self):
         return self.name
-
-   
-
 
 
 def product_pre_save_reciever(sender, instance, *args, **kwargs):
